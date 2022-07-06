dcd139adb6a1cbdea3f117517916f59969687873
Oli Rooker <oliver.rooker@gmail.com>
2022-07-06 15:13:11 +0100

    initial commit

---
+++ 220704-roman-numeral-converter.js
@@ -0,0 +1,10 @@
+/*
+Write a program that converts Roman numerals to their decimal equivalent.
+
+Further Challenge: write a program to convert decimal to Roman Numerals. Can the two programmes share any functions?
+
+Further Challenge: write a suite of tests to back up your code.
+
+Additional Further Challenge: Can you follow TDD principles for your development?
+
+*/
