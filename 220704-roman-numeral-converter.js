66e91c42fb93969196a8cac464e2f63a2ecfadc7
Oli Rooker <oliver.rooker@gmail.com>
2022-07-06 20:56:53 +0100

    set up tests and dependencies

--- 220704-roman-numeral-converter.js
+++ 220704-roman-numeral-converter.js
@@ -6,5 +6,8 @@ Further Challenge: write a program to convert decimal to Roman Numerals. Can the
 Further Challenge: write a suite of tests to back up your code.
 
 Additional Further Challenge: Can you follow TDD principles for your development?
-
 */
+
+export const romanNumeralConverter = () => {
+
+}
---
+++ __tests__/220704-roman-numeral-converter.test.js
@@ -0,0 +1,7 @@
+const { romanNumeralConverter } = require('../220704-roman-numeral-converter')
+
+describe('romanNumeralConverter() takes roman numerals and converts them into decimals', () => {
+    test('Do something', () => {
+        expect().toEqual()
+    });
+});
