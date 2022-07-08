d85214abf398712a73c5208741c83765831213e4
Oli Rooker <oliver.rooker@gmail.com>
2022-07-08 16:55:57 +0100

    add convet number to numeral

--- 220704-roman-numeral-converter.js
+++ 220704-roman-numeral-converter.js
@@ -8,8 +8,35 @@ Further Challenge: write a suite of tests to back up your code.
 Additional Further Challenge: Can you follow TDD principles for your development?
 */
 
-const romanNumeralConverter = (numeral) => {
-    return numeral
+const lookup = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
+}
+
+let numeralBuilder = ''
+// could be defined without type so it's flexible? Eg let builder;
+
+const romanNumeralConverter = (num) => {
+    let max = 0
+    for (let [numeral, decimal] of Object.entries(lookup)) {
+        if (num >= decimal && decimal >= max) {
+            numeralBuilder += numeral
+            max = decimal
+            num -= decimal
+        }
+    }
+    return num === 0 ? numeralBuilder : romanNumeralConverter(num)
 }
 
 module.exports = { romanNumeralConverter }
